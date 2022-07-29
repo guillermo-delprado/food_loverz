22333954983ce4218d84593a58e9520e3fb8b777
guillermo-delprado <galfonso.delprado@gmail.com>
2022-07-29 15:04:02 -0500

    Changes in Reviews form

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   resources :reviews
   devise_for :users
-  get 'home/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  root "home#index"
+  root "reviews#index"
 end
